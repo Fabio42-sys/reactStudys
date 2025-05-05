2a070460cd2f5e79c9227271740c8a95164a05e7
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2025-05-05 16:06:05 +0000

    feat: header created and fix search users

--- src/components/search-user.jsx
+++ src/components/search-user.jsx
@@ -1,6 +1,6 @@
 import { FaSearch } from "react-icons/fa";
 
-function searchUser({setFilter}) {
+function searchUser({setFiltro}) {
     return (
         <div className="search-area">
           <input type="text" placeholder="Search for your user" onChange={(e) => setFiltro(e.target.value)} />
---
+++ src/header.jsx
@@ -0,0 +1,20 @@
+import { FaCog, FaUserCircle, FaReact } from "react-icons/fa";
+import "./header.css";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <p>
+            Set, delete and edit your users. created using react <FaReact />
+        </p>
+        <div className="icons">
+          <FaCog />
+          <FaUserCircle />
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
